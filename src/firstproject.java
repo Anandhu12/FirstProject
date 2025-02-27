ed2d041c254b3a1fbfd97448fc2591eac896d196
Anandhu Manoj <67273000+Anandhu12@users.noreply.github.com>
2025-02-28 00:59:33 +0100

    commit-2.java

--- src/firstproject.java
+++ src/firstproject.java
@@ -2,5 +2,7 @@ public class firstproject {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("My name is Anandhu Manoj");
+        System.out.println("I am currently studying JAVA");
+        
     }
 }
